520455d57afe57b8f11099f294f6b4ff2486aad4
ABOUFOUDAYL <ABOUFOUDAYL@users.noreply.github.com>
2026-06-28 02:59:00 +0100

    fix: correction matcher middleware

--- middleware.ts
+++ middleware.ts
@@ -146,6 +146,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webpo$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
